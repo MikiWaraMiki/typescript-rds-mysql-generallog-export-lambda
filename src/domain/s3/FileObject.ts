fcda7190e223be98255bbb2d8d241438d86f870f
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-08-02 21:10:28 +0900

    fix fileBody

--- src/domain/s3/FileObject.ts
+++ src/domain/s3/FileObject.ts
@@ -1,19 +1,24 @@
+import { FileBody } from '@/domain/s3/FileBody'
 import { FileKeyName } from '@/domain/s3/FileKeyName'
 import { SSE_TYPE, SSE_TYPE_ENUM } from '@/domain/s3/SSEType'
 import { STORAGE_CLASS } from '@/domain/s3/StorageClass'
 
+// TODO: 必要になったらストレージクラスの変更等のメソッドを拡充
 export class FileObject {
   private storageClass: STORAGE_CLASS
   private fileName: FileKeyName
+  private fileBody: FileBody
   private serverSideEncryption: SSE_TYPE | undefined
 
   constructor(
     storageClass: STORAGE_CLASS,
     fileName: FileKeyName,
+    fileBody: FileBody,
     isSSEEnable: boolean,
   ) {
     this.storageClass = storageClass
     this.fileName = fileName
+    this.fileBody = fileBody
     this.serverSideEncryption = isSSEEnable ? SSE_TYPE_ENUM.AES : undefined
   }
 
@@ -27,5 +32,13 @@ export class FileObject {
 
   algorithmOfSSE(): string {
     return this.serverSideEncryption ?? ''
+  }
+
+  storageClassValue(): string {
+    return this.storageClass
+  }
+
+  body(): string {
+    return this.fileBody.data()
   }
 }
---
+++ src/domain/storage/TempFileRepository.ts
@@ -0,0 +1,3 @@
+export interface TempFileRepository {
+  convertBase64DataFromFile(filePath: string): string
+}
