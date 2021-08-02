93904dcea33e6d9ca7723db87575a09baa5e4e70
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-08-02 20:13:52 +0900

    fix 現段階で不要なメソッドを削除

--- src/domain/rds/LogFile.ts
+++ src/domain/rds/LogFile.ts
@@ -1,19 +1,23 @@
 import { LogFileName } from '@/domain/rds/LogFileName'
 import { LOG_FILE_TYPE } from '@/domain/rds/LogFileType'
+import { LogLastWrittenTime } from '@/domain/rds/LogLastWrittenTime'
 
 export class LogFile {
   private type: string
   private name: LogFileName
+  private lastWrittenTime: LogLastWrittenTime
   private size: number
 
-  constructor(type: string, name: LogFileName, size: number) {
+  constructor(
+    type: string,
+    name: LogFileName,
+    lastWrittenTime: LogLastWrittenTime,
+    size: number,
+  ) {
     this.type = type
     this.name = name
+    this.lastWrittenTime = lastWrittenTime
     this.size = size
-  }
-
-  static generateGeneralLog(name: LogFileName, size: number): LogFile {
-    return new LogFile(LOG_FILE_TYPE.GENERAL, name, size)
   }
 
   fileName(): string {
